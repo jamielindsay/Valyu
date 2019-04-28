338c251440d6bf345b99be1ba1f9c989a87278e2
jamielindsay <jamie.lindsay.vi@gmail.com>
2019-04-28 16:08:03 +0100

    Adjusted styling

--- src/navbars/navBarDetail.js
+++ src/navbars/navBarDetail.js
@@ -3,19 +3,14 @@ import "./navBarDetail.css";
 
 export default class NavBarDetail extends Component {
   backButton() {
-    return <div className="navIcon">BACK</div>;
-  }
-
-  statsButton() {
-    return <div className="navIcon">STATS</div>;
+    return <img className="navIcon" src="icons/back-nav.svg" alt="back" />;
   }
 
   render() {
     return (
       <div className="navBar">
         {this.backButton()}
-        <div>Rate Details</div>
-        {this.statsButton()}
+        <div className="navTitle">Rate Details</div>
       </div>
     );
   }
